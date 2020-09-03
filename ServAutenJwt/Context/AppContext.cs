e8330dfc0fb248bd8ccca31138a462e65018ca91
Manoel Neto <manoelcontatosi@gmail.com>
2020-09-03 09:56:49 -0300

    Desenvolvimento processo autenticacao

---
+++ ServAutenJwt/Context/AppContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServAutenJwt.Context
+{
+    public class AppContext : IdentityDbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+        public AppContext()
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.
+                       GetConnectionString("DefaultConnection");
+
+                optionsBuilder.UseMySql(connectionString);
+            }
+        }
+    }
+}
